098768b7a7e2c25fc9f3071b84315efd0b4b5ac5
rotsee <leo_wallentin@hotmail.com>
2015-08-24 15:54:54 +0200

    minify using grunt

---
+++ Gruntfile.js
@@ -0,0 +1,20 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    uglify: {
+      build: {
+        src: 'src/thenmap.js',
+        dest: 'build/thenmap-1.0.0.min.js'
+      }
+    }
+  });
+
+  // Load the plugin that provides the "uglify" task.
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+
+  // Default task(s).
+  grunt.registerTask('default', ['uglify']);
+
+};
