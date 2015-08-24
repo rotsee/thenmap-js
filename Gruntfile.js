25df2e227ad7ee4eeed1cb1df662d823b4fd8eea
rotsee <leo_wallentin@hotmail.com>
2015-08-24 17:45:45 +0200

    include tabletop in build/thenmap.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,25 +3,31 @@ module.exports = function(grunt) {
   // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+    concat: {
+      dist: {
+        src: ['src/thenmap.js', 'js/tabletop.js'],
+        dest: 'src/thenmap.tmp.js',
+      }
+    },
     uglify: {
       build: {
-        src: 'src/thenmap.js',
+        src: 'src/thenmap.tmp.js',
         dest: 'build/thenmap-<%= pkg.version %>.min.js'
       }
     },
     copy: {
       main: {
-        src: 'src/thenmap.js',
+        src: 'src/thenmap.tmp.js',
         dest: 'build/thenmap-<%= pkg.version %>.js'
       }
     },
   });
 
-  // Load the plugin that provides the "uglify" task.
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-concat');
 
   // Default task(s).
-  grunt.registerTask('default', ['uglify', 'copy']);
+  grunt.registerTask('default', ['concat', 'uglify', 'copy']);
 
 };
