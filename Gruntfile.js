4cb8c71c661f726c222c58843cd178d21a68dacc
rotsee <leo_wallentin@hotmail.com>
2015-08-27 15:26:03 +0200

    add grunt jobs

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,18 +3,32 @@ module.exports = function(grunt) {
   // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+
+    str2js: {
+      CSS: { 'src/styles.js': ['src/styles.css']}
+    },
+
+    includereplace: {
+      dist: {
+        src: 'src/thenmap.js',
+        dest: 'src/thenmap.tmp.js'
+      }
+    },
+
     concat: {
       dist: {
-        src: ['src/thenmap.js', 'js/tabletop.js'],
+        src: ['src/thenmap.tmp.js', 'js/tabletop.js'],
         dest: 'src/thenmap.tmp.js',
       }
     },
+
     uglify: {
       build: {
         src: 'src/thenmap.tmp.js',
         dest: 'build/thenmap-<%= pkg.version %>.min.js'
       }
     },
+
     copy: {
       main: {
         src: 'src/thenmap.tmp.js',
@@ -26,8 +40,10 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-include-replace');
+  grunt.loadNpmTasks('grunt-string-to-js');
 
   // Default task(s).
-  grunt.registerTask('default', ['concat', 'uglify', 'copy']);
+  grunt.registerTask('default', ['str2js', 'includereplace', 'concat', 'uglify', 'copy']);
 
 };
